9355cefb0beee4df337a89872cc97ba0240d79de
phpip <phpip@qq.com>
2015-08-26 17:15:59 +0800

    新增：不同角色进入后台显示不同的菜单

--- coreframe/app/core/admin/index.php
+++ coreframe/app/core/admin/index.php
@@ -23,7 +23,13 @@ final class index extends WUZHI_admin {
         require COREFRAME_ROOT.'languages/'.$lang.'/admin_menu.lang.php';
         $_panels = $panels = array();
         $result = $this->db->get_list('menu', 'pid<20 AND display=1', '*', 0, 1000, 0, 'sort ASC', '', 'menuid');
+        //限制非超管用户的访问菜单
+        if ($_SESSION['role']!=1) {
+            $admin_private = $this->db->get_list('admin_private', 'chk=1 AND role='.$_SESSION['role'],'*', 0, 1000, 0, '', '', 'id');
+            $admin_private_keys = array_keys($admin_private);
+        }
         foreach($result as $key=>$r) {
+            if($_SESSION['role']!=1 && !in_array($key,$admin_private_keys)) continue;
             if($key<20) {
                 $panels[$key] = $r;
             } else {
