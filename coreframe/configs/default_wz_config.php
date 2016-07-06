75f766a44c5d33cda89db1f29ec778f1192a08a8
wuzhicms <phpip@qq.com>
2016-07-06 19:12:31 +0800

    修复：安装缺失文件

---
+++ coreframe/configs/default_wz_config.php
@@ -0,0 +1,7 @@
+<?php
+if(!defined('WWW_ROOT')) {
+    //定义网站根目录路径
+    define('WWW_ROOT','/workspace/wwwroot/wuzhicms_v2/src/www/');
+}
+define('ADMIN_LOG',1);//开启后台管理日志
+define('EDIT_TPL',1);//是否允许在线编辑模板
