b782a2f187643ae70649afa26f704359a97efad1
wuzhicms <phpip@qq.com>
2016-07-07 23:20:18 +0800

    移动SQL

--- upgrade/3.0/3/upgrade.php
+++ upgrade/3.0/3/upgrade.php
@@ -1,13 +1,13 @@
 <?php
 //+----------------------------------------------------------------------
-// | wuzhicms [ ��ָ������վ���ݹ���ϵͳ ]
+// | wuzhicms [ 五指互联网站内容管理系统 ]
 // | Copyright (c) 2014-2015 http://www.wuzhicms.com All rights reserved.
 // | Licensed ( http://www.wuzhicms.com/licenses/ )
 // | Author: wangcanjia <phpip@qq.com>
 //+----------------------------------------------------------------------
 defined('IN_WZ') or exit('No direct script access allowed');
 
-//  ���û�Ա��Ȩ�ޣ����˻�Ա ��Ҫ���� ���οͻ�Ա����Ҫ�ж��Ƿ���Ȩ��
+//  设置会员组权限：个人会员 需要升级 ，游客会员组需要判断是否有权限
 //attachment
 $fields_arr = $db->get_fields('attachment');
 if(!in_array('userid',$fields_arr)) {
@@ -27,5 +27,5 @@ if(!in_array('sys_name',$fields_arr)) {
 	$db->query("ALTER TABLE `wz_member` ADD `sys_name` TINYINT(1) NOT NULL DEFAULT '0'");
 }
 
-//�����²˵�����
+//最后更新菜单缓存
 load_class('cache_menu');
