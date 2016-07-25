b744c4032a07956992b07ce038901cdfb2ff0ca9
wuzhicms <phpip@qq.com>
2016-07-25 11:20:53 +0800

    优化：推荐位按照不同模版显示

--- coreframe/app/content/admin/block.php
+++ coreframe/app/content/admin/block.php
@@ -29,7 +29,7 @@ class block extends WUZHI_admin {
 	}
 
     public function listing() {
-        $where = array('siteid'=>$this->siteid);
+        $where = array('siteid'=>$this->siteid,'tplid'=>TPLID);
         $page = intval($GLOBALS['page']);
         $result = $this->db->get_list('block', $where, '*', 0, 20, $page,'blockid DESC');
         $pages = $this->db->pages;
