a2560b46d3fb3e781866183e2b6ce0285f1a0a88
wuzhicms <phpip@qq.com>
2016-09-26 11:23:15 +0800

    新增：数据类型字段：
    浮点型 decimal(4,3) - 一般用于评分换算,默认5.000
    浮点型 decimal(4,1) - 一般用于评分,默认5.0

--- coreframe/app/core/fields/db.php
+++ coreframe/app/core/fields/db.php
@@ -23,97 +23,103 @@ switch($field_type) {
 		$maxlength = min($maxlength, 255);
 		$sql = "ALTER TABLE `$db_table` $sqltype CHAR( $maxlength ) NOT NULL DEFAULT '$initialise'";
 		$this->db->query($sql);
-	break;
+		break;
 
 	case 'mediumtext':
 		$this->db->query("ALTER TABLE `$db_table` $sqltype MEDIUMTEXT NOT NULL");
-	break;
-	
+		break;
+
 	case 'text':
 		$this->db->query("ALTER TABLE `$db_table` $sqltype TEXT NOT NULL");
-	break;
+		break;
 
 	case 'tinyint':
 		$initialise = intval($initialise);
 		$this->db->query("ALTER TABLE `$db_table` $sqltype TINYINT ".($min_value >= 0 ? 'UNSIGNED' : '')." NOT NULL DEFAULT '$initialise'");
-	break;
+		break;
 
 	case 'smallint':
 		$initialise = intval($initialise);
 		$this->db->query("ALTER TABLE `$db_table` $sqltype SMALLINT ".($min_value >= 0 ? 'UNSIGNED' : '')." NOT NULL DEFAULT '$initialise'");
-	break;
+		break;
 
 	case 'mediumint':
 		$initialise = intval($initialise);
 		$this->db->query("ALTER TABLE `$db_table` $sqltype MEDIUMINT ".($min_value >= 0 ? 'UNSIGNED' : '')." NOT NULL DEFAULT '$initialise'");
-	break;
+		break;
 
 	case 'int':
 		$initialise = intval($initialise);
 		$this->db->query("ALTER TABLE `$db_table` $sqltype INT ".($min_value >= 0 ? 'UNSIGNED' : '')." NOT NULL DEFAULT '$initialise'");
-	break;
+		break;
 
 	case 'number':
 		$initialise = $decimaldigits == 0 ? intval($initialise) : floatval($initialise);
 		$sql = "ALTER TABLE `$db_table` $sqltype ".($decimaldigits == 0 ? 'INT' : 'FLOAT')." ".($min_value >= 0 ? 'UNSIGNED' : '')." NOT NULL DEFAULT '$initialise'";
 		$this->db->query($sql);
-	break;
+		break;
 
 	case 'date':
 		$this->db->query("ALTER TABLE `$db_table` $sqltype DATE NULL");
-	break;
-	
+		break;
+
 	case 'datetime':
 		$this->db->query("ALTER TABLE `$db_table` $sqltype DATETIME NULL");
-	break;
-	
+		break;
+
 	case 'timestamp':
 		$this->db->query("ALTER TABLE `$db_table` $sqltype TIMESTAMP NOT NULL");
-	break;
+		break;
 	//金币
 	case 'coin':
 		$initialise = intval($initialise);
 		$this->db->query("ALTER TABLE `$db_table` $sqltype smallint(5) unsigned NOT NULL default '$initialise'");
-	break;
+		break;
 
-    case 'money':
-		$this->db->query("ALTER TABLE `$db_table` $sqltype decimal(10,2) NOT NULL default '0.00'");
-	break;
+	case 'money':
+		$this->db->query("ALTER TABLE `$db_table` $sqltype decimal(10,2) NOT NULL default '$initialise'");
+		break;
 	case 'money1':
-		$this->db->query("ALTER TABLE `$db_table` $sqltype decimal(10,1) NOT NULL default '0.0'");
-	break;
-    case 'map':
-        if($action=='add') {
-            $sqltype1 = "ADD `{$field}_zoom`";
-            $sqltype2 = "ADD `{$field}_x`";
-            $sqltype3 = "ADD `{$field}_y`";
-        } else {
-            $sqltype1 = "CHANGE `{$oldfield}_zoom` `{$field}_zoom`";
-            $sqltype2 = "CHANGE `{$oldfield}_x` `{$field}_x`";
-            $sqltype3 = "CHANGE `{$oldfield}_y` `{$field}_y`";
-        }
+		$this->db->query("ALTER TABLE `$db_table` $sqltype decimal(10,1) NOT NULL default '$initialise'");
+		break;
+	case 'money2':
+		$this->db->query("ALTER TABLE `$db_table` $sqltype decimal(4,3) NOT NULL default '5.000'");
+		break;
+	case 'money3':
+		$this->db->query("ALTER TABLE `$db_table` $sqltype decimal(4,1) NOT NULL default '5.0'");
+		break;
+	case 'map':
+		if($action=='add') {
+			$sqltype1 = "ADD `{$field}_zoom`";
+			$sqltype2 = "ADD `{$field}_x`";
+			$sqltype3 = "ADD `{$field}_y`";
+		} else {
+			$sqltype1 = "CHANGE `{$oldfield}_zoom` `{$field}_zoom`";
+			$sqltype2 = "CHANGE `{$oldfield}_x` `{$field}_x`";
+			$sqltype3 = "CHANGE `{$oldfield}_y` `{$field}_y`";
+		}
 
-        $sql = "ALTER TABLE `$db_table` $sqltype1 TINYINT( 2 ) UNSIGNED NOT NULL DEFAULT '8'";
-        $this->db->query($sql);
-        $sql = "ALTER TABLE `$db_table` $sqltype2 decimal(10,6) NOT NULL DEFAULT '0'";
-        $this->db->query($sql);
-        $sql = "ALTER TABLE `$db_table` $sqltype3 decimal(10,6) NOT NULL DEFAULT '0'";
-        $this->db->query($sql);
-        break;
-    case 'price_group':
-        if($action=='add') {
-            $sqltype1 = "ADD `{$field}`";
-            $sqltype2 = "ADD `{$field}_old`";
-        } else {
-            $sqltype1 = "CHANGE `{$oldfield}` `{$field}_zoom`";
-            $sqltype2 = "CHANGE `{$oldfield}_old` `{$field}_old`";
-        }
+		$sql = "ALTER TABLE `$db_table` $sqltype1 TINYINT( 2 ) UNSIGNED NOT NULL DEFAULT '8'";
+		$this->db->query($sql);
+		$sql = "ALTER TABLE `$db_table` $sqltype2 decimal(10,6) NOT NULL DEFAULT '0'";
+		$this->db->query($sql);
+		$sql = "ALTER TABLE `$db_table` $sqltype3 decimal(10,6) NOT NULL DEFAULT '0'";
+		$this->db->query($sql);
+		break;
+	case 'price_group':
+		if($action=='add') {
+			$sqltype1 = "ADD `{$field}`";
+			$sqltype2 = "ADD `{$field}_old`";
+		} else {
+			$sqltype1 = "CHANGE `{$oldfield}` `{$field}_zoom`";
+			$sqltype2 = "CHANGE `{$oldfield}_old` `{$field}_old`";
+		}
 
-        $sql = "ALTER TABLE `$db_table` $sqltype1 decimal(8,2) NOT NULL DEFAULT '8'";
-        $this->db->query($sql);
-        $sql = "ALTER TABLE `$db_table` $sqltype2 decimal(8,2) NOT NULL DEFAULT '0'";
-        $this->db->query($sql);
-        break;
+		$sql = "ALTER TABLE `$db_table` $sqltype1 decimal(8,2) NOT NULL DEFAULT '8'";
+		$this->db->query($sql);
+		$sql = "ALTER TABLE `$db_table` $sqltype2 decimal(8,2) NOT NULL DEFAULT '0'";
+		$this->db->query($sql);
+		break;
 	case 'linkage':
 		if($action=='add') {
 			$sqltype1 = "ADD `{$field}`";
--- coreframe/app/core/fields/number/form_setting.php
+++ coreframe/app/core/fields/number/form_setting.php
@@ -4,12 +4,14 @@
         <td>字段类型</td>
         <td>
             <select name="setting[fieldtype]" onchange="javascript:fieldtype_setting(this.value);">
-				                <option value="int" <?php if(output($setting,'fieldtype')=='int') echo 'selected';?>>整数 INT(10)</option>
+                <option value="int" <?php if(output($setting,'fieldtype')=='int') echo 'selected';?>>整数 INT(10)</option>
                 <option value="tinyint" <?php if(output($setting,'fieldtype')=='tinyint') echo 'selected';?>>整数 TINYINT(3)</option>
                 <option value="smallint" <?php if(output($setting,'fieldtype')=='smallint') echo 'selected';?>>整数 SMALLINT(5)</option>
                 <option value="mediumint" <?php if(output($setting,'fieldtype')=='mediumint') echo 'selected';?>>整数 MEDIUMINT(8)</option>
-                <option value="money1" <?php if(output($setting,'fieldtype')=='money1') echo 'selected';?>>浮点型 decimal(10,1)</option>
-                <option value="money" <?php if(output($setting,'fieldtype')=='money') echo 'selected';?>>浮点型 decimal(10,2)</option>
+                <option value="money1" <?php if(output($setting,'fieldtype')=='money1') echo 'selected';?>>浮点型 decimal(10,1) - 例如:1.0 </option>
+                <option value="money" <?php if(output($setting,'fieldtype')=='money') echo 'selected';?>>浮点型 decimal(10,2) - 例如: 1.00</option>
+                <option value="money2" <?php if(output($setting,'fieldtype')=='money2') echo 'selected';?>>浮点型 decimal(4,3) - 一般用于评分换算,默认5.000</option>
+                <option value="money3" <?php if(output($setting,'fieldtype')=='money3') echo 'selected';?>>浮点型 decimal(4,1) - 一般用于评分,默认5.0</option>
             </select> <span id="minnumber" style="display:none"><input type="radio" name="setting[minnumber]" value="1" <?php if(output($setting,'minnumber')==1) echo 'checked';?>/> <font color='red'>正整数</font> <input type="radio" name="setting[minnumber]" value="-1" <?php if(output($setting,'minnumber')==-1) echo 'checked';?>/> 整数</span>
         </td>
     </tr>
