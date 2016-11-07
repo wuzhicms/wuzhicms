610cd49a4d5197a0741ae5a5911aea20f56dd9e0
wuzhicms <phpip@qq.com>
2016-11-07 18:36:43 +0800

    新增：短信验证码测试，测试账号都是 test时，可通过1111通过验证

--- coreframe/app/sms/sms.php
+++ coreframe/app/sms/sms.php
@@ -11,83 +11,97 @@ defined('IN_WZ') or exit('No direct script access allowed');
  */
 class sms{
 	public function __construct() {
-        $this->db = load_class('db');
+		$this->db = load_class('db');
 	}
 
-    /**
-     * 发送短信
-     */
-    public function sendsms() {
-        //验证 页面验证码是否正确
-        //插入相关信息
-        /**
-        1、使用安全图片验证码（网站）
-        2、单IP的请求次数限定 （网站）（APP）
-        3、单用户动态短信请求间隔时长限制（网站）（APP）
-        4、  同一手机号次数限定 （网站）（APP）
-         */
-        $uid = get_cookie('_uid');
-        $mobile = $GLOBALS['mobile'];
-        if(!preg_match('/^(?:13\d{9}|15[0|1|2|3|5|6|7|8|9]\d{8}|17[0|1|2|3|5|6|7|8|9]\d{8}|18[0|1|2|3|5|6|7|8|9]\d{8}|14[5|7]\d{8})$/',$mobile)) {
-            exit('201');
-        }
-        $checkcode = $GLOBALS['checkcode'];
-        if($checkcode=='') exit('202');
-        load_class('session');
-        if(strtolower($_SESSION['code']) != strtolower($checkcode)) exit('202');
-        $_SESSION['code'] = '';
+	/**
+	 * 发送短信(手机短信验证码、图片验证码)
+	 */
+	public function sendsms() {
+		//验证 页面验证码是否正确
+		//插入相关信息
+		/**
+		1、使用安全图片验证码（网站）
+		2、单IP的请求次数限定 （网站）（APP）
+		3、单用户动态短信请求间隔时长限制（网站）（APP）
+		4、  同一手机号次数限定 （网站）（APP）
+		 */
+		$config = get_cache('sms_config','sms');
+		$uid = get_cookie('_uid');
+		$mobile = $GLOBALS['mobile'];
+		if(!preg_match('/^(?:13\d{9}|15[0|1|2|3|5|6|7|8|9]\d{8}|17[0|1|2|3|5|6|7|8|9]\d{8}|18[0|1|2|3|5|6|7|8|9]\d{8}|14[5|7]\d{8})$/',$mobile)) {
+			exit('201');
+		}
+		$checkcode = $GLOBALS['checkcode'];
+		if($checkcode=='') exit('202');
+		load_class('session');
+		if(strtolower($_SESSION['code']) != strtolower($checkcode)) exit('202');
+		$_SESSION['code'] = '';
 
-        $endtime = SYS_TIME-120;
+		$endtime = SYS_TIME-120;
 
-        if($_SESSION['et'] && $_SESSION['et']>$endtime) {
-            exit('204');
-        } else {
-            $_SESSION['et'] = SYS_TIME;
-        }
+		if($_SESSION['et'] && $_SESSION['et']>$endtime) {
+			if($config['sms_uid']!='test') {
+				exit('204');
+			}
+		} else {
+			$_SESSION['et'] = SYS_TIME;
+		}
+		if(isset($GLOBALS['isreg']) && $GLOBALS['isreg']) {//是否为注册，验证手机是否已注册
+			$reg_data = $this->db->get_one('member', array('mobile' => $mobile),'uid');
+			if($reg_data) {
+				exit('206');
+			}
+		}
+		$posttime = SYS_TIME-86400;
+		$ip = get_ip();
+		$where = "`ip`='$ip' AND `posttime`>$posttime";
+		$num = $this->db->count_result('sms_checkcode',$where);
+		if($num>200 && $config['sms_uid']!='test') {//单IP 24小时内最大请求次数限定
+			exit('203');
+		}
 
-        $posttime = SYS_TIME-86400;
-        $ip = get_ip();
-        $where = "`ip`='$ip' AND `posttime`>$posttime";
-        $num = $this->db->count_result('sms_checkcode',$where);
-        if($num>200) {//单IP 24小时内最大请求次数限定
-            exit('203');
-        }
+		//单用户动态短信请求间隔时长限制 ,根据手机号码判断是否为一个用户
+		$where = "`mobile`='$mobile'";
+		$r = $this->db->get_one('sms_checkcode',$where, '*', 0,'id DESC' );
 
-        //单用户动态短信请求间隔时长限制 ,根据手机号码判断是否为一个用户
-        $where = "`mobile`='$mobile'";
-        $r = $this->db->get_one('sms_checkcode',$where, '*', 0,'id DESC' );
-
-        if($r['posttime']>$endtime) {//120 秒之内连续请求
-            exit('204');
-        }
+		if($r['posttime']>$endtime && $config['sms_uid']!='test') {//120 秒之内连续请求
+			exit('204');
+		}
 
 
-        //同一手机号次数限定
-        $where = "`mobile`='$mobile' AND `posttime`>=$posttime";
-        $num = $this->db->count_result('sms_checkcode',$where);
-        if($num>200) {//同一手机号次数限定 24小时内最大请求次数限定
-            exit('205');
-        }
+		//同一手机号次数限定
+		$where = "`mobile`='$mobile' AND `posttime`>=$posttime";
+		$num = $this->db->count_result('sms_checkcode',$where);
+		if($num>200 && $config['sms_uid']!='test') {//同一手机号次数限定 24小时内最大请求次数限定
+			exit('205');
+		}
 //验证通过
-        $code = rand(1000,9999);
 
-        $sendsms = load_class('sms','sms');
-        $code = rand(1000,9999);
-        $formdata = array();
-        $formdata['mobile'] = $mobile;
-        $formdata['uid'] = $uid;
-        $formdata['posttime'] = SYS_TIME;
-        $formdata['code'] = $code;
-        $formdata['ip'] = $ip;
-          $this->db->insert('sms_checkcode', $formdata);
+		$sendsms = load_class('sms','sms');
+		if($config['sms_uid']!='test') {
+			$code = rand(1000,9999);
+		} else {
+			$code = 1111;
+		}
 
-        $returnstr = $sendsms->send_sms($mobile, $code, 1); //发送短信
-        if($sendsms->statuscode==0) {
-            exit('0');
-        } else {
-            echo $returnstr;
-        }
-
+		$formdata = array();
+		$formdata['mobile'] = $mobile;
+		$formdata['uid'] = $uid;
+		$formdata['posttime'] = SYS_TIME;
+		$formdata['code'] = $code;
+		$formdata['ip'] = $ip;
+		$this->db->insert('sms_checkcode', $formdata);
+		if($config['sms_uid']!='test') {
+			$returnstr = $sendsms->send_sms($mobile, $code, 1); //发送短信
+			if($sendsms->statuscode==0) {
+				exit('0');
+			} else {
+				echo $returnstr;
+			}
+		} else {
+			exit('0');
+		}
 	}
 }
 ?>
