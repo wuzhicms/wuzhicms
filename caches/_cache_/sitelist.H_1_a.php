82cf33af737c5eecb98b751616e1b3dee6611ddb
wuzhicms <phpip@qq.com>
2017-02-22 22:40:17 +0800

    缓存：站点配置

--- caches/_cache_/sitelist.H_1_a.php
+++ caches/_cache_/sitelist.H_1_a.php
@@ -6,5 +6,20 @@
     'name' => '默认站点',
     'logo' => '/res/images/userimg.jpg',
     'url' => 'http://dev.wuzhicms.com/',
+    'html_root' => '',
+    'setting' => 'a:9:{s:8:"sitename";s:33:"五指CMS网站内容管理系统";s:4:"logo";s:25:"/res/images/head_logo.png";s:12:"seo_keywords";s:52:"五指CMS网站内容管理系统,cms,wuzhicms系统";s:15:"seo_description";s:33:"五指CMS网站内容管理系统";s:9:"copyright";s:100:"Copyright © 2015 北京五指互联科技有限公司 All Rights Reserved<br>京ICP备14036160号-1";s:8:"statcode";s:0:"";s:16:"access_authority";s:1:"0";s:5:"close";s:1:"0";s:12:"close_reason";s:36:"站点升级中，请稍后访问！";}',
+    'baidu_site' => '',
+    'baidu_token' => '',
+  ),
+  2 => 
+  array (
+    'siteid' => '2',
+    'name' => '演示站点2',
+    'logo' => 'http://dev.wuzhicms.com/uploadfile/2016/11/03/20161103192100tjCJeM.png',
+    'url' => 'http://dev2.wuzhicms.com/',
+    'html_root' => '',
+    'setting' => 'a:8:{s:8:"sitename";s:7:"站点2";s:4:"logo";s:25:"/res/images/head_logo.png";s:12:"seo_keywords";s:52:"五指CMS网站内容管理系统,cms,wuzhicms系统";s:15:"seo_description";s:33:"五指CMS网站内容管理系统";s:9:"copyright";s:100:"Copyright © 2015 北京五指互联科技有限公司 All Rights Reserved<br>京ICP备14036160号-1";s:8:"statcode";s:0:"";s:5:"close";s:1:"0";s:12:"close_reason";s:36:"站点升级中，请稍后访问！";}',
+    'baidu_site' => '',
+    'baidu_token' => '',
   ),
 )?>
