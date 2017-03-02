a4200cb56630acba054f2a3b512fa68e0c5ffca3
wuzhicms <wcj@wuzhicms.com>
2017-03-02 19:43:04 +0800

    修复：来源链接显示bug

--- caches/model/content_format.H_1_a.php
+++ caches/model/content_format.H_1_a.php
@@ -126,37 +126,25 @@ class form_format {
 		return $value;
 	}
 
-    private function copyfrom($field, $value) {
-        if(is_numeric($value)) {
-            $r = $this->db->get_one('copyfrom',array('fromid'=>$value));
-            if(defined('IN_PACKAGE')) {
-                if($r['logo']) {
-                return '<a href="#/resources/'.$r['name'].'"><span class="logo_ly"><img src="'.$r['logo'].'"></span> '.$r['name'].'</a>';
-                } else {
-                return '<a href="#/resources/'.$r['name'].'">'.$r['name'].'</a>';
-                }
-            } else {
-                $cid = intval($GLOBALS['cid']);
-                $cr = $this->db->get_one('category',array('cid'=>$cid),'siteid');
-                if($r['logo']) {
-                    return '<a href="/index.php?f=copyfrom&id='.$value.'&siteid='.$cr['siteid'].'" target="_blank"><span class="logo_ly"><img src="'.$r['logo'].'"></span> '.$r['name'].'</a>';
-                } else {
-                    return '<a href="/index.php?f=copyfrom&id='.$value.'&siteid='.$cr['siteid'].'" target="_blank">'.$r['name'].'</a>';
-                }
-            }
-
-
-        } elseif(is_string($value)) {
-            if(strpos($value,'|')===false) {
-                return $value;
-            } else {
-                $values = explode('|',$value);
-                $values[1] = 'http://'.ltrim($values[1],'http://');
-                return '<a href="'.$values[1].'" target="_blank">'.$values[0]."</a>";
-            }
-            return $value;
-        }
-    }
+	private function copyfrom($field, $value){
+		if (is_numeric($value)) {
+			$r = $this->db->get_one('copyfrom', array('fromid' => $value));
+			if ($r['logo']) {
+				return '<a href="'.$r['url'].'" target="_blank"><span class="logo_ly"><img src="' . $r['logo'] . '"></span> ' . $r['name'] . '</a>';
+			} else {
+				return '<a href="'.$r['url'].'" target="_blank">' . $r['name'] . '</a>';
+			}
+		} elseif (is_string($value)) {
+			if (strpos($value, '|') === false) {
+				return $value;
+			} else {
+				$values = explode('|', $value);
+				$values[1] = 'http://' . ltrim($values[1], 'http://');
+				return '<a href="' . $values[1] . '" target="_blank">' . $values[0] . "</a>";
+			}
+			return $value;
+		}
+	}
 
 	private function datetime($field, $value) {
 		$setting = $this->fields[$field]['setting'];
