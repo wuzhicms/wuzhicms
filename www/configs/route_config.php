0cee5b4a0fd6eefcedcb18e31fda8a0cc92e52ae
wuzhicms <wcj@wuzhicms.com>
2017-03-02 11:13:00 +0800

    默认配置

--- www/configs/route_config.php
+++ www/configs/route_config.php
@@ -3,5 +3,4 @@ defined('IN_WZ') OR exit('No direct script access allowed');
 
 return array(
 	'default'=>array('m'=>'content', 'f'=>'index', 'v'=>'init'),
-	//'iiis2.wuzhicms.cn'=>array('m'=>'content', 'f'=>'index', 'v'=>'init','_get'=>array('siteid'=>2)),
 );
