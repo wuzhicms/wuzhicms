5e64cf519eb44a1fd782ea45ee1b8302a534e8ba
wuzhicms <wcj@wuzhicms.com>
2017-03-10 00:24:26 +0800

    修复升级SQL

--- upgrade/4.0/0/upgrade.php
+++ upgrade/4.0/0/upgrade.php
@@ -27,10 +27,11 @@ $fields_arr = $db->get_fields('linkage_data');
 if(!in_array('isgroup',$fields_arr)) {
 	$db->query("ALTER TABLE `wz_linkage_data` ADD `isgroup` TINYINT(1) UNSIGNED NOT NULL DEFAULT '0'");
 }
+//特殊：改字段名称
 $fields_arr = $db->get_fields('tag');
-if(!in_array('linkage',$fields_arr)) {
+if(in_array('linkage',$fields_arr)) {
 	$db->query("ALTER TABLE `wz_tag` CHANGE `linkage` `linkageid` SMALLINT(5) UNSIGNED NOT NULL COMMENT '类别'");
-	$db->query("INSERT INTO `wz_menu` (`menuid`, `pid`, `name`, `m`, `f`, `v`, `data`, `sort`, `display`, `isopenid`) VALUES(307, 5, '模块管理', 'core', 'app', 'init', '', 307, 1, 0);");
+	$db->query("INSERT INTO `wz_menu` (`menuid`, `pid`, `name`, `m`, `f`, `v`, `data`, `sort`, `display`, `isopenid`) VALUES(307, 5, '模块管理', 'core', 'app', 'init', '', 307, 1, 0)");
 
 }
 
